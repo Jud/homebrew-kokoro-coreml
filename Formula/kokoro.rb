7d79199253c7c9612f9b51b6f5766c7ca8c78c5b
Judson Stephenson <Jud@users.noreply.github.com>
2026-03-23 12:10:40 -0500

    Fix: cd to libexec in wrapper so bundles are found

--- Formula/kokoro.rb
+++ Formula/kokoro.rb
@@ -12,12 +12,9 @@ class Kokoro < Formula
     libexec.install "kokoro"
     Dir["*.bundle"].each { |b| libexec.install b }
 
-    # Symlink bundles next to the bin wrapper so NSBundle can find them
-    Dir[libexec/"*.bundle"].each { |b| ln_s b, bin/File.basename(b) }
-
     (bin/"kokoro").write <<~SH
       #!/bin/bash
-      exec "#{libexec}/kokoro" "$@"
+      cd "#{libexec}" && exec ./kokoro "$@"
     SH
   end
 
